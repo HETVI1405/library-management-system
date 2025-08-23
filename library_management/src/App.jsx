a31674932e29209a64c57980f52fac14e9b935f2
GHAGDEEPPRAVIN <deepghag48@gmail.com>
2025-08-23 14:17:17 +0530

    improveDashboard

--- library_management/src/App.jsx
+++ library_management/src/App.jsx
@@ -36,8 +36,7 @@ export default function App() {
 {/* <Dashboard /> */}
 {/* <Books /> */}
 <div style={{backgroundColor:"#72acea",width:"100%",height:"60px",display:"flex",justifyContent:"space-between",alignItems:"center",padding:"0px 50px"}}>
-  <h5 style={{color:"white"}} >National Digital 
-      Library </h5>
+  <h5 style={{color:"white"}} >National Digital Library </h5>
       <div className="left" style={{color:"whitesmoke"}}> <i class="ri-admin-fill"></i> Welcome Admin... </div>
 </div>
 <div style={{display:"flex",width:"100%",height:"100vh"}}>
